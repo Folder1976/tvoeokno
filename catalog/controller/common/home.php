48ca560d4fcd7400f038dcb5e9e078674706653f
Sergey Kotlyarov <folder.list@gmail.com>
2017-12-02 20:00:25 +0200

    folder

--- catalog/controller/common/home.php
+++ catalog/controller/common/home.php
@@ -16,6 +16,51 @@ class ControllerCommonHome extends Controller {
 		$data['footer'] = $this->load->controller('common/footer');
 		$data['header'] = $this->load->controller('common/header');
 
+		
+		$this->load->model('extension/module');
+
+		$data['modules'] = array();
+
+		$modules = $this->model_design_layout->getLayoutModules(1, 'content_top');
+
+		foreach ($modules as $module) {
+			$part = explode('.', $module['code']);
+
+			if (isset($part[0]) && $this->config->get($part[0] . '_status')) {
+				$module_data = $this->load->controller('extension/module/' . $part[0]);
+
+				if ($module_data) {
+					$data['modules'][] = $module_data;
+				}
+			}
+
+			if (isset($part[1])) {
+				$setting_info = $this->model_extension_module->getModule($part[1]);
+		
+				if ($setting_info && $setting_info['status']) {
+					$output = $this->load->controller('extension/module/' . $part[0], $setting_info);
+
+					if ($output) {
+						$data['modules'][$part[0].'-'.$part[1]] = $output;
+					}
+				}
+			}
+		}
+	
+		$this->load->model('blog/blog');
+ 		$this->load->model('blog/blog_category');
+ 		
+ 		$res = $this->model_blog_blog_category->getBlogCategories();
+ 		
+ 		foreach($res as $index => $row){
+ 			$data['blogs'][$row['keyword']] = $row;
+ 			$data['blogs'][$row['keyword']]['blogs'] = $this->model_blog_blog->getBlogsByBlogCategoryId($row['blog_category_id']);
+ 		}
+ 		
+ 		$this->load->model('catalog/manufacturer');
+ 		$data['manufacturers'] = $this->model_catalog_manufacturer->getManufacturers();
+
+		
 		$this->response->setOutput($this->load->view('common/home', $data));
 	}
 }
