8c712b02094d71817bdc0b387ec8f20355d52559
Sergey Kotlyarov <folder.list@gmail.com>
2017-12-21 18:09:16 +0200

    home products

--- catalog/controller/common/home.php
+++ catalog/controller/common/home.php
@@ -72,8 +72,124 @@ class ControllerCommonHome extends Controller {
  			$data['blogs'][$row['keyword']]['blogs'] = $this->model_blog_blog->getBlogsByBlogCategoryId($row['blog_category_id']);
  		}
  		
+		$_GET['blog_id'] = 80;
+ 		$data['comment_blog'] = $this->model_blog_blog->getBlog(80);
+ 		
+ 		$results = $this->model_blog_blog->getCommentsByBlogId(80, 0, 10);
+  
+ 		foreach ($results as $result) {
+         		$data['comments'][] = array(
+         			'name'     => $result['name'],
+ 					'email'     => $result['email'],
+ 					'comment'       => strip_tags($result['comment']),
+         			'date_added' => date($this->language->get('date_format_short'), strtotime($result['date_added']))
+         		);
+       		}
+
+	
  		$this->load->model('catalog/manufacturer');
  		$data['manufacturers'] = $this->model_catalog_manufacturer->getManufacturers();
+		
+		
+		$this->load->model('catalog/category');
+			$this->load->model('catalog/product');
+		
+			$data['products'] = array();
+			$data['categorys'] = array();
+
+			$filter_data = array(
+				'filter_category_id' => 0
+			);
+
+			$product_total = $this->model_catalog_product->getTotalProducts($filter_data);
+
+			$results = $this->model_catalog_product->getProducts($filter_data);
+
+			
+			foreach ($results as $result) {
+			
+				if ($this->customer->isLogged() || !$this->config->get('config_customer_price')) {
+					$price = $this->currency->format($this->tax->calculate($result['price'], $result['tax_class_id'], $this->config->get('config_tax')), $this->session->data['currency']);
+				} else {
+					$price = false;
+				}
+
+				if ((float)$result['special']) {
+					$special = $this->currency->format($this->tax->calculate($result['special'], $result['tax_class_id'], $this->config->get('config_tax')), $this->session->data['currency']);
+				} else {
+					$special = false;
+				}
+
+				if ($this->config->get('config_tax')) {
+					$tax = $this->currency->format((float)$result['special'] ? $result['special'] : $result['price'], $this->session->data['currency']);
+				} else {
+					$tax = false;
+				}
+
+				if ($this->config->get('config_review_status')) {
+					$rating = (int)$result['rating'];
+				} else {
+					$rating = false;
+				}
+
+				$data['categorys'][$result['category_id']]['products'][] = $result['product_id'];
+				
+				$options = array();
+
+				foreach ($this->model_catalog_product->getProductOptions($result['product_id']) as $option) {
+					$product_option_value_data = array();
+	
+					foreach ($option['product_option_value'] as $option_value) {
+						if (!$option_value['subtract'] || ($option_value['quantity'] > 0)) {
+							if ((($this->config->get('config_customer_price') && $this->customer->isLogged()) || !$this->config->get('config_customer_price')) && (float)$option_value['price']) {
+								$price = $this->currency->format($this->tax->calculate($option_value['price'], $result['tax_class_id'], $this->config->get('config_tax') ? 'P' : false), $this->session->data['currency']);
+							} else {
+								$price = false;
+							}
+	
+							$product_option_value_data[] = array(
+								'product_option_value_id' => $option_value['product_option_value_id'],
+								'option_value_id'         => $option_value['option_value_id'],
+								'name'                    => $option_value['name'],
+								'image'                   => $this->model_tool_image->resize($option_value['image'], 50, 50),
+								'price'                   => $price,
+								'price_prefix'            => $option_value['price_prefix']
+							);
+						}
+					}
+	
+					$options[] = array(
+						'product_option_id'    => $option['product_option_id'],
+						'product_option_value' => $product_option_value_data,
+						'option_id'            => $option['option_id'],
+						'name'                 => $option['name'],
+						'type'                 => $option['type'],
+						'value'                => $option['value'],
+						'required'             => $option['required']
+					);
+				}
+				
+				$data['products'][$result['product_id']] = array(
+					'product_id'  => $result['product_id'],
+					'option'		=> $options,
+					'name'        => $result['name'],
+					'description' => utf8_substr(strip_tags(html_entity_decode($result['description'], ENT_QUOTES, 'UTF-8')), 0, $this->config->get($this->config->get('config_theme') . '_product_description_length')) . '..',
+					'price'       => $price,
+					'special'     => $special,
+					'tax'         => $tax,
+					'minimum'     => $result['minimum'] > 0 ? $result['minimum'] : 1,
+					'rating'      => $result['rating'],
+					'href'        => $this->url->link('product/product', 'product_id=' . $result['product_id'] . '')
+				);
+			}
+
+			foreach($data['categorys'] as $_category_id => $row){
+				
+				$data['categorys'][$_category_id] = $this->model_catalog_category->getCategory($_category_id);
+				$data['categorys'][$_category_id]['products'] = $row['products'];
+				
+			}
+		
 		
 		$this->response->setOutput($this->load->view('common/home', $data));
 	}
